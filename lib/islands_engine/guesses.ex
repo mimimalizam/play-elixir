f936dedb13a57a0234c1db3773c5d55e3edc4ae9
Milana <milana.stojadinov@gmail.com>
2018-08-01 22:48:05 +0200

    Keep track of guesses - hit and miss

--- lib/islands_engine/guesses.ex
+++ lib/islands_engine/guesses.ex
@@ -1,5 +1,5 @@
 defmodule IslandsEngine.Guesses do
-  alias __MODULE__
+  alias IslandsEngine.{Coordinate, Guesses}
 
   @enforce_keys [:hits, :misses]
   defstruct [:hits, :misses]
@@ -10,5 +10,11 @@ defmodule IslandsEngine.Guesses do
 
   def new(), do:
     %Guesses{hits: MapSet.new(), misses: MapSet.new()}
+
+  def add(%Guesses{} = guesses, :hit, %Coordinate{} = coordinate), do:
+   update_in(guesses.hits, &MapSet.put(&1, coorddinate))
+
+  def add(%Guesses{} = guesses, :misses, %Coordinate{} = coordinate), do:
+   update_in(guesses.misses, &MapSet.put(&1, coorddinate))
 
 end
